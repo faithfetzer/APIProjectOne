f6bed10c8b1a4a5299b721f1639f14fd3a2bde67
Faith Fetzer <faithfetzer@Faiths-Air.attlocal.net>
2021-05-11 16:55:21 -0400

    Filled table with corresponding information

--- script.js
+++ script.js
@@ -33,21 +33,54 @@ function displayResults(json) {
         let resultsTable = document.createElement('table');
         resultsData.appendChild(resultsTable);
         resultsTable.classList.add('resultsData');
+        resultsTable.style.border = "1px solid black";
         let tableInternal = document.querySelector('.resultsData');
-        resultsTable.innerHTML='<tr><th>Race Name</th><th>Race Date</th><th>Circuit Name</th><th>City</th><th>Country</th></tr>'
-        let tableRow =document.createElement('tr');
-        let tableData = document.createElement('td');
-        let link = document.createElement('a');
-        for({raceName, date} of races){
-            tableInternal.appendChild(tableRow);
-            // resultsTable.tableRow();
-            console.log(raceName, date);
+        for(let i=0; i<=races.length; i++){
+            let tableRow =document.createElement('tr');
+            const currentRace= races[i-1];
+            // let link = document.createElement('a');
+            tableInternal.appendChild(tableRow).classList.add('internal-row', 'row'+i);
+        // }
             // tableRow.tableData();
             // tableRow.firstChild.innerHTML = raceName;
             // tableRow.appendChild.innerHTML = '<td>date</td>';
-        }
-        };
-        console.log(resultsData);
+            // let currentRowClass = 1;
+            // let currentRow = document.querySelector('.currentRowClass');
+            // console.log(currentRow);
+            // for(let j=0; j<5; j++){
+            // let currentRow = document.querySelector('.resultsData tr:nth-child(i)');
+            // console.log(currentRow);
+            let tableData =document.createElement('td');
+            let currentRow = document.querySelector('.row'+i);
+            // let link = document.createElement('a');
+            // currentRow.appendChild(tableData);
+            // };
+            // currentRowClass++;
+            if(i==0){
+            let headerRow = document.querySelector('.row0');
+            headerRow.appendChild(tableData);
+            headerRow.innerHTML = '<td>Round</td><td>Race Name</td><td>Race Date</td><td>City</td><td>Country</td><td>Circuit Name</td>';
+            
+            } else {
+                    // const currentRow = document.querySelector('.row'+i);
+                currentRow.innerHTML='<td></td><td></td><td></td><td></td><td></td><td></td>';
+                    // console.log(currentRace);         
+                let roundNumber = currentRow.firstChild;
+                roundNumber.textContent = currentRace.round;
+                let raceName = roundNumber.nextSibling;
+                raceName.textContent = currentRace.raceName;
+                let raceDate = raceName.nextSibling;
+                raceDate.textContent = currentRace.date;
+                let raceCity = raceDate.nextSibling;
+                raceCity.textContent = currentRace.Circuit.Location.locality;
+                let raceCountry = raceCity.nextSibling;
+                raceCountry.textContent = currentRace.Circuit.Location.country;
+                let circuitName = raceCountry.nextSibling;
+                circuitName.textContent = currentRace.Circuit.circuitName;
+            }
+            
     }
-    
+    // console.log(resultsData);
+}
 
+}
